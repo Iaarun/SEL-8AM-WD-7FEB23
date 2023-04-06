c9bda9e8dbe7be91ea15a6f21c75abb5b7964d05
Iaarun <automationbyaarun@gmail.com>
2023-04-06 09:03:26 +0530

    adding selenium code

---
+++ JavaWD8AMFEB23/src/seleniumScripts/Testscripts.java
@@ -0,0 +1,60 @@
+package seleniumScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Testscripts {
+	int a=10;
+	WebDriver driver;
+	public static void main(String[] args) {
+		Testscripts ts = new Testscripts();
+		ts.launchBrowseronchoice("firefox");
+		ts.launchApplication();
+	}
+	
+	
+	public void launchBrowseronchoice(String browser) {
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		
+	}
+	
+	public void launchApplication() {
+	   driver.get("https://live.guru99.com/");
+	   
+	   //get the open application url
+	  String url=   driver.getCurrentUrl();
+	  System.out.println(url);
+	  
+	  //get the title of the page
+	  String title=  driver.getTitle();
+	  System.out.println(title);
+	  
+	  //get the id of the tab
+	  String tabid= driver.getWindowHandle();
+	  System.out.println(tabid);
+	  
+	  //get the page source
+	 String src=  driver.getPageSource();
+	 System.out.println(src);
+	  
+	}
+	
+	
+	public void launchBrowsers() {
+		//to launch chrome browser
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().window().minimize();
+		driver.close();
+		
+	//	driver.quit();
+	}
+
+}
